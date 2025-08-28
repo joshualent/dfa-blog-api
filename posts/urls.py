bd88b73b6dc13e060628590d14a4768d797d57cc
joshualent <joshualent83@gmail.com>
2025-08-28 15:39:02 -0400

    add schema and documentation

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,20 @@
 from django.urls import path
+from rest_framework.routers import SimpleRouter
 
-from .views import PostList, PostDetail
+from .views import UserViewSet, PostViewSet
 
-urlpatterns = [
-    path("<int:pk>/", PostDetail.as_view(), name="post_detail"),
-    path("", PostList.as_view(), name="post_list"),
-]
+router = SimpleRouter()
+router.register("users", UserViewSet, basename="users")
+router.register("", PostViewSet, basename="posts")
+
+urlpatterns = router.urls
+
+
+# from .views import postlist, postdetail, userlist, userdetail
+
+# urlpatterns = [
+#     path("users/", userlist.as_view(), name="user_list"),
+#     path("users/<int:pk>/", userdetail.as_view(), name="user_detail"),
+#     path("<int:pk>/", postdetail.as_view(), name="post_detail"),
+#     path("", postlist.as_view(), name="post_list"),
+# ]
